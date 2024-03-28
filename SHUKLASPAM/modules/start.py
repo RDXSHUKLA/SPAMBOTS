13e7424ea6ecb18bb7210736e870f0c426418b24
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-03-29 01:56:48 +0530

    Update start.py

--- SHUKLASPAM/modules/start.py
+++ SHUKLASPAM/modules/start.py
@@ -1,47 +1,44 @@
-# © @SHIVANSH474
-from telethon import __version__, events, Button
+from telethon import events, Button
+from config import MK1, MK2, MK3, MK4, MK5, MK6, MK7, MK8, MK9, MK10
+from SHUKLASPAM.modules.help import *
+import telethon
 
-from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
+PythonButton = [
+        [
+        Button.inline("𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦", data="help_back")
+        ],
+        [
+        Button.url("𝗖𝗛𝗔𝗡𝗡𝗘𝗟", "https://t.me/SHIVANSH474"),
+        Button.url("𝗦𝗨𝗣𝗣𝗢𝗥𝗧", "https://t.me/mastiwithfriendsx")
+        ],
+        [
+        Button.url("𝗥𝗘𝗣𝗢", "https://github.com/itzshukla/STRANGER-SPAM-X/fork")
+        ]
+        ]
 
 
-START_BUTTON = [
-    [
-        Button.inline("• 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 •", data="help_back")
-    ],
-    [
-        Button.url("•  𝗖𝗛𝗔𝗡𝗡𝗘𝗟 •", "https://t.me/SHIVANSH474"),
-        Button.url("• 𝗦𝗨𝗣𝗣𝗢𝗥𝗧 •", "https://t.me/mastiwithfriendsx")
-    ],
-    [
-        Button.url("• 𝗥𝗘𝗣𝗢 •", "https://github.com/itzshukla/STRANGER-SPAM-X/fork")
-    ]
-]
-
-
-@X1.on(events.NewMessage(pattern="/start"))
-@X2.on(events.NewMessage(pattern="/start"))
-@X3.on(events.NewMessage(pattern="/start"))
-@X4.on(events.NewMessage(pattern="/start"))
-@X5.on(events.NewMessage(pattern="/start"))
-@X6.on(events.NewMessage(pattern="/start"))
-@X7.on(events.NewMessage(pattern="/start"))
-@X7.on(events.NewMessage(pattern="/start"))
-@X8.on(events.NewMessage(pattern="/start"))
-@X9.on(events.NewMessage(pattern="/start"))
-@X10.on(events.NewMessage(pattern="/start"))
+@MK1.on(events.NewMessage(pattern="/start"))
+@MK2.on(events.NewMessage(pattern="/start"))
+@MK3.on(events.NewMessage(pattern="/start"))
+@MK4.on(events.NewMessage(pattern="/start"))
+@MK5.on(events.NewMessage(pattern="/start"))
+@MK6.on(events.NewMessage(pattern="/start"))
+@MK7.on(events.NewMessage(pattern="/start"))
+@MK7.on(events.NewMessage(pattern="/start"))
+@MK8.on(events.NewMessage(pattern="/start"))
+@MK9.on(events.NewMessage(pattern="/start"))
+@MK10.on(events.NewMessage(pattern="/start"))
 async def start(event):              
     if event.is_private:
         AltBot = await event.client.get_me()
-        bot_name = AltBot.first_name
-        bot_id = AltBot.id
-        TEXT = f"**ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ : [𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛](https://t.me/SHIVANSH39)**\n\n"
-        TEXT += f"» **sᴛʀᴀɴɢᴇʀʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
-        TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
-        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
+        BotName = AltBot.first_name
+        BotId = AltBot.id
+        TEXT = f"**𝗛𝗘𝗬 [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝗜 𝗔𝗠  [{BotName}](tg://user?id={BotId})**\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» **✦ 𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗗 𝗕𝗬 :~ [𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛](https://t.me/SHIVANSH39)**\n\n"
+        TEXT += f"» **𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥 𝗦𝗣𝗔𝗠 𝗩𝗘𝗥𝗦𝗜𝗢𝗡 :** `3.2`\n"
+        TEXT += f"» **𝗧𝗘𝗟𝗘𝗧𝗛𝗢𝗡 𝗩𝗘𝗥𝗦𝗜𝗢𝗡:** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
-                    event.chat_id,
-                    "https://graph.org/file/c6a2ed96648fd03377dc9.jpg",
-                    caption=TEXT, 
-                    buttons=START_BUTTON
-                )
+                event.chat_id,
+                "https://graph.org/file/c6a2ed96648fd03377dc9.jpg",
+                caption=TEXT, 
+                buttons=PythonButton)
