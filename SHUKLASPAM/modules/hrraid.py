4392588183bac79bc7fed7a90657d06fac05f251
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-04-08 12:20:48 +0530

    Update hrraid.py

--- SHUKLASPAM/modules/hrraid.py
+++ SHUKLASPAM/modules/hrraid.py
@@ -6,7 +6,7 @@ from random import choice
 from telethon import events
 
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
-from SHUKLASPAM.data import HRAID,  SHASHANK
+from SHUKLASPAM.data import HRAID, SHASHANK
 
 HREPLY_RAID = []
 
